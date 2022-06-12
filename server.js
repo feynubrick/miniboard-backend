a8bef68cfcb641792f66ae59f84a4ef13039a32b
Seung-Yoon Oh <feynubrick@gmail.com>
2022-06-12 18:31:14 +0900

    change environment variable names

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 const app = express();
-const port = process.env.HTTP_SERVER_PORT;
+const port = process.env.MINIBOARD_MAIN_BACKEND_HTTP_PORT;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
