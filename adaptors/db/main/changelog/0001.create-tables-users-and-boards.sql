2e8496bb0a4bd159453b63309ab674484f0ddc99
Seung-Yoon Oh <feynubrick@gmail.com>
2022-06-19 14:05:33 +0900

    change initial schema

--- adaptors/db/main/changelog/0001.create-tables-users-and-boards.sql
+++ adaptors/db/main/changelog/0001.create-tables-users-and-boards.sql
@@ -3,12 +3,12 @@
 --changeset feynubrick:1 dbms:postgresql
 CREATE TABLE users (
       id SERIAL PRIMARY KEY,
-      hash_id text UNIQUE,
+      hash_id text UNIQUE NOT NULL,
       oauth_id text,
       oauth_platform varchar(10),
       refresh_token text,
-      cookie text UNIQUE,
-      created_at timestamp WITH TIME ZONE DEFAULT now(),
+      cookie text UNIQUE NOT NULL,
+      created_at timestamp WITH TIME ZONE DEFAULT now() NOT NULL,
       last_login_at timestamp WITH TIME ZONE DEFAULT NULL,
       unique (oauth_id, oauth_platform)
 );
@@ -17,12 +17,12 @@ CREATE TABLE users (
 --changeset feynubrick:2 dbms:postgresql
 CREATE TABLE boards (
       id SERIAL PRIMARY KEY,
-      hash_id text,
+      hash_id text NOT NULL,
       content text NOT NULL,
-      writer_id integer REFERENCES users(id) ON DELETE CASCADE,
-      created_at timestamp WITH TIME ZONE DEFAULT now(),
+      writer_id integer REFERENCES users(id) ON DELETE CASCADE NOT NULL,
+      created_at timestamp WITH TIME ZONE DEFAULT now() NOT NULL,
       edited_at timestamp WITH TIME ZONE DEFAULT NULL,
-      num_read integer DEFAULT 0,
-      unique (id, hash_id)
+      num_read integer DEFAULT 0 NOT NULL,
+      unique (writer_id, hash_id)
 );
 --rollback DROP TABLE boards;
